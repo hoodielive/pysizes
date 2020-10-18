51cedfd3644674638edfc761573a66eb918b76d3
hoodielive <hoodielive@gmail.com>
2020-10-18 19:06:53 -0400

    Review

---
+++ newstuff.py
@@ -0,0 +1,11 @@
+egregori = [ 911, 404, 187, 28 ]
+
+for hex in egregori:
+    print(hex)
+
+count = 3
+
+while count > 0:
+    print('Howdy There ladies and gents: ', count)
+    count -= 1
+print('Boom!')
